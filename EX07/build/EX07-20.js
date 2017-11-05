04fecbc8a3e46d5462a8a54dc5cafd6daff6402f
KunhoLee <gunho1020@gmail.com>
2017-11-05 22:15:24 +0900

    예제 07-19, 07-20
    모듈 (export default 사용)

---
+++ EX07/build/EX07-20.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var _EX = require('./utils/EX07-19');
+
+var _EX2 = _interopRequireDefault(_EX);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log(_EX2.default.add(4, 5));
+console.log(_EX2.default.multiply(4, 5));
---
+++ EX07/src/EX07-20.js
@@ -0,0 +1,4 @@
+import calc from './utils/EX07-19';
+
+console.log(calc.add(4, 5));
+console.log(calc.multiply(4, 5));
