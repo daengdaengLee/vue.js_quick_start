ce929e7d272f7100e49a0706da51e2846a8b5929
KunhoLee <gunho1020@gmail.com>
2017-11-09 11:47:58 +0900

    컴포넌트에서의 스타일
    범위 CSS는 부모 컴포넌트에서 자식 컴포넌트로 상속된다.

--- EX09/scopedCSS/src/components/Child1.vue
+++ EX09/scopedCSS/src/components/Child1.vue
@@ -1,12 +1,17 @@
 <template>
-    <div class="main">
+    <div class="main test">
+        {{ msg }}
+        <child11 />
         {{ msg }}
     </div>
 </template>
 
 <script>
+import Child11 from './Child11.vue';
+
 export default {
     name: 'child1',
+    components: { Child11 },
     data() {
         return {
             msg: 'Child1'
@@ -19,5 +24,10 @@ export default {
 .main {
     border: solid 1px black;
     background-color: yellow;
+}
+.test {
+    padding: 10px;
+    text-decoration: underline;
+    border: solid 1px black;
 }
 </style>
