982ed230e72df2aa2e165746becac7da3218e2f1
KunhoLee <gunho1020@gmail.com>
2017-11-09 11:35:09 +0900

    컴포넌트에서의 스타일
    전역 css 스타일의 문제점

---
+++ EX09/scopedCSS/src/components/Child1.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="main">
+        {{ msg }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'child1',
+    data() {
+        return {
+            msg: 'Child1'
+        };
+    }
+};
+</script>
+
+<style>
+.main {
+    border: solid 1px black;
+    background-color: yellow;
+}
+</style>
