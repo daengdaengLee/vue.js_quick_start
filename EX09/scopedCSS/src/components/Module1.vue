a9cd7fe9096d10de62840d1d84eedbfafbcdf2ad
KunhoLee <gunho1020@gmail.com>
2017-11-09 12:06:08 +0900

    컴포넌트에서의 스타일
    CSS 모듈

---
+++ EX09/scopedCSS/src/components/Module1.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button v-bind:class="[$style.hand, $style.test]"> CSS Module을 적용한 버튼 </button>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        console.log('[CSS MODULE TEST] : ', this.$style);
+    }
+};
+</script>
+
+<style module>
+.hand {
+    cursor: pointer;
+    background-color: purple;
+    color: yellow;
+}
+.test {
+    font-style: italic;
+}
+</style>
