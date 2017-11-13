46e6468cd473b9efa78766193235eebd7001bb05
KunhoLee <gunho1020@gmail.com>
2017-11-13 11:20:41 +0900

    연락처 애플리케이션 예제
    예제 10-16: src/App.vue의 기본 골격

--- EX10/contactsapp/src/App.vue
+++ EX10/contactsapp/src/App.vue
@@ -1,28 +1,79 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+    <div id="container">
+        <div class="page-header">
+            <h1 class="text-center">연락처 관리 애플리케이션</h1>
+            <p>(Dynamic Component + EventBus + Axios)</p>
+        </div>
+        <component :is="currentView" :contact="contact"></component>
+        <contact-list :contactlist="contactlist"></contact-list>
+        <paginate ref="pagebuttons"
+            :page-count="totalpage"
+            :page-range="7"
+            :margin-pages="3"
+            :click-handler="pageChanged"
+            :prev-text="'이전'"
+            :next-text="'다음'"
+            :container-class="'pagination'"
+            :page-class="'page-item'">
+        </paginate>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Vue from 'vue';
+import Paginate from 'vuejs-paginate';
+
+import ContactList from './components/ContactList.vue';
+import AddContact from './components/AddContact.vue';
+import UpdateContact from './components/UpdateContact.vue';
+import UpdatePhoto from './components/UpdatePhoto.vue';
+
+import CONF from './Config';
+import eventBus from './EventBus';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'app',
+    components: {
+        ContactList,
+        AddContact,
+        UpdateContact,
+        UpdatePhoto,
+        Paginate
+    },
+    data() {
+        return {
+            currentView: null,
+            contact: {
+                no: 0,
+                name: '',
+                tel: '',
+                address: '',
+                photo: ''
+            },
+            contactlist: {
+                pageno: 1,
+                pagesize: CONF.PAGESIZE,
+                totalcount: 0,
+                contacts: []
+            }
+        };
+    },
+    mounted() {},
+    computed: {},
+    methods: {},
+    watch: {}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+@import url('https://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.css');
+
+#container {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
