807d5678b9ac6ea3584b5772866ff6c23bdac928
KunhoLee <gunho1020@gmail.com>
2017-11-14 14:40:45 +0900

    연락처 애플리케이션 예제 예제 10-22: src/components/AddContact.vue 예제 10-23: src/components/UpdateContact.vue 예제 10-24: src/components/ContactForm.vue

---
+++ EX10/contactsapp/src/components/AddContact.vue
@@ -0,0 +1,12 @@
+<template>
+    <contact-form mode="add" />
+</template>
+
+<script>
+import ContactForm from './ContactForm.vue';
+
+export default {
+    name: 'add-contact',
+    components: { ContactForm }
+};
+</script>
---
+++ EX10/contactsapp/src/components/UpdateContact.vue
@@ -0,0 +1,13 @@
+<template>
+    <contact-form mode="update" v-bind:contact="contact" />
+</template>
+
+<script>
+import ContactForm from './ContactForm.vue';
+
+export default {
+    name: 'update-contact',
+    components: { ContactForm },
+    props: ['contact']
+};
+</script>
