7b646cb993f2f5ddee7bfbc9154f54fd9c5f1d53
KunhoLee <gunho1020@gmail.com>
2017-11-22 15:06:51 +0900

    예제 11-09 : src/main.js
    저장소를 참조하고 Vue 인스턴스를 만들 때 store 옵션 추가

--- EX11/countryapp/src/main.js
+++ EX11/countryapp/src/main.js
@@ -1,7 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
 
+/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    store,
+    el: '#app',
+    render: h => h(App)
+});
