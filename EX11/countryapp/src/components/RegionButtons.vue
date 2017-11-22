4b96850d5faad98c8f1f6df486596fc7eb22cbc6
KunhoLee <gunho1020@gmail.com>
2017-11-22 15:25:43 +0900

    예제 11-11: src/components/RegionButtons.vue

---
+++ EX11/countryapp/src/components/RegionButtons.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <button
+            class="region"
+            v-for="region in regions"
+            v-bind:key="region"
+            v-bind:class="isSelected(region)"
+            v-on:click="changeRegion({ region: region })"
+        >
+            {{ region }}
+        </button>
+    </div>
+</template>
+
+<script>
+import _ from 'lodash';
+import { mapGetters, mapMutations } from 'vuex';
+import Constant from '../Constant';
+
+export default {
+    name: 'region-buttons',
+    computed: mapGetters([
+        'regions', 'currentRegion'
+    ]),
+    methods: _.extend(
+        {
+            isSelected(region) {
+                if (region === this.currentRegion) return { selected: true };
+                return { selected: false };
+            }
+        },
+        mapMutations([
+            Constant.CHANGE_REGION
+        ])
+    )
+};
+</script>
+
+<style>
+button.region {
+    text-align: center;
+    width: 80px;
+    margin: 2px;
+    border: solid 1px gray;
+}
+button.selected {
+    background-color: purple;
+    color: aqua;
+}
+</style>
