e945fd6d245714230d5798d4dcc322895f3c5d97
KunhoLee <gunho1020@gmail.com>
2017-11-22 16:09:46 +0900

    예제 11-13: src/store/index.js
    store 객체에 actions 정의

--- EX11/todolistapp2/src/store/index.js
+++ EX11/todolistapp2/src/store/index.js
@@ -25,6 +25,20 @@ const store = new Vuex.Store({
         },
         [Constant.DELETE_TODO]: (state, payload) => {
             state.todolist.splice(payload.index, 1);
+        },
+        actions: {
+            [Constant.ADD_TODO]: ({ commit }, payload) => {
+                console.log('### addTodo!!!');
+                commit(Constant.ADD_TODO, payload);
+            },
+            [Constant.DONE_TOGGLE]: ({ commit }, payload) => {
+                console.log('### doneToggle!!!');
+                commit(Constant.DONE_TOGGLE, payload);
+            },
+            [Constant.DELETE_TODO]: ({ commit }, payload) => {
+                console.log('### deleteTodo!!!');
+                commit(Constant.DELETE_TODO, payload);
+            }
         }
     }
 });
