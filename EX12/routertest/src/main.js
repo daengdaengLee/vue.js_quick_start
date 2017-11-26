09f8bac660e069099729203b7e243128f438c21c
KunhoLee <gunho1020@gmail.com>
2017-11-26 10:44:56 +0900

    main.js에서 Vue가 VueRouter 사용하도록 설정

--- EX12/routertest/src/main.js
+++ EX12/routertest/src/main.js
@@ -1,7 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
 
+Vue.use(VueRouter);
+
+/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    el: '#app',
+    render: h => h(App)
+});
