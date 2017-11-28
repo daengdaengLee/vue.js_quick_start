4263161c70812c019ce1043a13a6295bca35e90b
KunhoLee <gunho1020@gmail.com>
2017-11-28 23:10:50 +0900

    예제 12-28: src/store/actions.js
    store 액션 수정

--- EX12/contactsapp/src/store/actions.js
+++ EX12/contactsapp/src/store/actions.js
@@ -2,51 +2,6 @@ import contactAPI from '../api/ContactsAPI';
 import Constant from '../constant';
 
 export default {
-    [Constant.ADD_CONTACT_FORM]: (store) => {
-        store.commit(Constant.ADD_CONTACT_FORM);
-    },
-    [Constant.ADD_CONTACT]: (store) => {
-        contactAPI.addContact(store.state.contact).then((response) => {
-            if (response.data.status === 'success') {
-                store.dispatch(Constant.CANCEL_FORM);
-                store.dispatch(Constant.FETCH_CONTACTS, { pageno: 1 });
-            } else {
-                /* eslint-disable no-console */
-                console.log(`연락처 추가 실패 : ${response.data}`);
-                /* eslint-enable no-console */
-            }
-        });
-    },
-    [Constant.EDIT_CONTACT_FORM]: (store, payload) => {
-        contactAPI.fetchContactOne(payload.no).then((response) => {
-            store.commit(Constant.EDIT_CONTACT_FORM, { contact: response.data });
-        });
-    },
-    [Constant.UPDATE_CONTACT]: (store) => {
-        const currentPageNo = store.state.contactlist.pageno;
-        contactAPI.updateContact(store.state.contact).then((response) => {
-            if (response.data.status === 'success') {
-                store.dispatch(Constant.CANCEL_FORM);
-                store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
-            } else {
-                /* eslint-disable no-console */
-                console.log(`연락처 변경 실패 : ${response.data}`);
-                /* eslint-enable no-console */
-            }
-        });
-    },
-    [Constant.EDIT_PHOTO_FORM]: (store, payload) => {
-        contactAPI.fetchContactOne(payload.no).then((response) => {
-            store.commit(Constant.EDIT_PHOTO_FORM, { contact: response.data });
-        });
-    },
-    [Constant.UPDATE_PHOTO]: (store, payload) => {
-        const currentPageNo = store.state.contactlist.pageno;
-        contactAPI.updatePhoto(payload.no, payload.file).then(() => {
-            store.dispatch(Constant.CANCEL_FORM);
-            store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
-        });
-    },
     [Constant.FETCH_CONTACTS]: (store, payload) => {
         let pageno;
         if (typeof payload === 'undefined' || typeof payload.pageno === 'undefined') {
@@ -60,8 +15,30 @@ export default {
             store.commit(Constant.FETCH_CONTACTS, { contactlist: response.data });
         });
     },
-    [Constant.CANCEL_FORM]: (store) => {
-        store.commit(Constant.CANCEL_FORM);
+    [Constant.ADD_CONTACT]: (store) => {
+        contactAPI.addContact(store.state.contact).then((response) => {
+            if (response.data.status === 'success') {
+                store.dispatch(Constant.FETCH_CONTACTS, { pageno: 1 });
+            } else {
+                console.log(`연락처 추가 실패 : ${response.data}`); // eslint-disable-line no-console
+            }
+        });
+    },
+    [Constant.UPDATE_CONTACT]: (store) => {
+        const currentPageNo = store.state.contactlist.pageno;
+        contactAPI.updateContact(store.state.contact).then((response) => {
+            if (response.data.status === 'success') {
+                store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
+            } else {
+                console.log(`연락처 변경 실패 : ${response.data}`); // eslint-disable-line no-console
+            }
+        });
+    },
+    [Constant.UPDATE_PHOTO]: (store, payload) => {
+        const currentPageNo = store.state.contactlist.pageno;
+        contactAPI.updatePhoto(payload.no, payload.file).then(() => {
+            store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
+        });
     },
     [Constant.DELETE_CONTACT]: (store, payload) => {
         const currentPageNo = store.state.contactlist.pageno;
@@ -69,7 +46,12 @@ export default {
             store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
         });
     },
-    [Constant.CHANGE_MODE]: (store, payload) => {
-        store.commit(Constant.CHANGE_MODE, { mode: payload.mode });
+    [Constant.FETCH_CONTACT_ONE]: (store, payload) => {
+        contactAPI.fetchContactOne(payload.no).then((response) => {
+            store.commit(Constant.FETCH_CONTACT_ONE, { contact: response.data });
+        });
+    },
+    [Constant.INITIALIZE_CONTACT_ONE]: (store) => {
+        store.commit(Constant.INITIALIZE_CONTACT_ONE);
     }
 };
