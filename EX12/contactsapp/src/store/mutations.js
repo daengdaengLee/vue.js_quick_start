42d04f71d1b9e0fe26a400fb6968d6f6f1676000
KunhoLee <gunho1020@gmail.com>
2017-11-28 23:00:59 +0900

    예제 12-27: src/store/mutations.js
    store 변이 변경

--- EX12/contactsapp/src/store/mutations.js
+++ EX12/contactsapp/src/store/mutations.js
@@ -2,31 +2,18 @@ import Constant from '../constant';
 
 // 상태를 변경하는 기능만을 뽑아서...
 export default {
-    [Constant.ADD_CONTACT_FORM]: (state) => {
+    [Constant.FETCH_CONTACTS]: (state, payload) => {
+        const storeState = state;
+        storeState.contactlist = payload.contactlist;
+    },
+    [Constant.FETCH_CONTACT_ONE]: (state, payload) => {
+        const storeState = state;
+        storeState.contact = payload.contact;
+    },
+    [Constant.INITIALIZE_CONTACT_ONE]: (state) => {
         const storeState = state;
         storeState.contact = {
             no: '', name: '', tel: '', address: '', photo: ''
         };
-        storeState.mode = 'add';
-        storeState.currentView = 'contactForm';
-    },
-    [Constant.CANCEL_FORM]: (state) => {
-        const storeState = state;
-        storeState.currentView = null;
-    },
-    [Constant.EDIT_CONTACT_FORM]: (state, payload) => {
-        const storeState = state;
-        storeState.contact = payload.contact;
-        storeState.mode = 'update';
-        storeState.currentView = 'contactForm';
-    },
-    [Constant.EDIT_PHOTO_FORM]: (state, payload) => {
-        const storeState = state;
-        storeState.contact = payload.contact;
-        storeState.currentView = 'updatePhoto';
-    },
-    [Constant.FETCH_CONTACTS]: (state, payload) => {
-        const storeState = state;
-        storeState.contactlist = payload.contactlist;
     }
 };
