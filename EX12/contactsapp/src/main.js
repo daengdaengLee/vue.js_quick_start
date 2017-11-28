8b02562c680211770264d0d8bf8919cc815a579c
KunhoLee <gunho1020@gmail.com>
2017-11-28 23:34:39 +0900

    예제 12-29: src/main.js
    라우트 정보 추가

--- EX12/contactsapp/src/main.js
+++ EX12/contactsapp/src/main.js
@@ -1,12 +1,45 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 import store from './store';
 
+import Home from './components/Home.vue';
+import About from './components/About.vue';
+import ContactList from './components/ContactList.vue';
+import ContactForm from './components/ContactForm.vue';
+import UpdatePhoto from './components/UpdatePhoto.vue';
+
+Vue.use(VueRouter);
 Vue.config.productionTip = false;
+
+const router = new VueRouter({
+    routes: [
+        { path: '/', redirect: '/home' },
+        { path: '/home', name: 'home', components: Home },
+        { path: '/about', name: 'about', components: About },
+        {
+            path: '/contacts',
+            name: 'contacts',
+            components: ContactList,
+            children: [
+                {
+                    path: 'add', name: 'addcontact', components: ContactForm
+                },
+                {
+                    path: 'update/:no', name: 'updatecontact', components: ContactForm, props: true
+                },
+                {
+                    path: 'photo/:no', name: 'updatephoto', components: UpdatePhoto, props: true
+                }
+            ]
+        }
+    ]
+});
 
 /* eslint-disable no-new */
 new Vue({
     store,
+    router,
     el: '#app',
     template: '<App/>',
     components: { App }
