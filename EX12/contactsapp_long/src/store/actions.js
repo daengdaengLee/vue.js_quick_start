0942f60ccefe7845c1c29ecd54c681968c11d243
KunhoLee <gunho1020@gmail.com>
2017-11-30 09:57:21 +0900

    예제 12-39: src/store/actions.js 변경
    지연 시간 발생 하는 지점에서 CHANGE_ISLOADING 호출

--- EX12/contactsapp_long/src/store/actions.js
+++ EX12/contactsapp_long/src/store/actions.js
@@ -2,6 +2,9 @@ import contactAPI from '../api/ContactsAPI';
 import Constant from '../constant';
 
 export default {
+    [Constant.CHANGE_ISLOADING]: (store, payload) => {
+        store.commit(Constant.CHANGE_ISLOADING, payload);
+    },
     [Constant.FETCH_CONTACTS]: (store, payload) => {
         let pageno;
         if (typeof payload === 'undefined' || typeof payload.pageno === 'undefined') {
@@ -12,11 +15,16 @@ export default {
         }
         const { pagesize } = store.state.contactlist;
 
+        store.dispatch(Constant.CHANGE_ISLOADING, { isloading: true });
+
         contactAPI.fetchContacts(pageno, pagesize).then((response) => {
             store.commit(Constant.FETCH_CONTACTS, { contactlist: response.data });
+            store.dispatch(Constant.CHANGE_ISLOADING, { isloading: false });
         });
     },
     [Constant.ADD_CONTACT]: (store) => {
+        store.dispatch(Constant.CHANGE_ISLOADING, { isloading: true });
+
         contactAPI.addContact(store.state.contact).then((response) => {
             if (response.data.status === 'success') {
                 store.dispatch(Constant.FETCH_CONTACTS, { pageno: 1 });
@@ -26,6 +34,8 @@ export default {
         });
     },
     [Constant.UPDATE_CONTACT]: (store) => {
+        store.dispatch(Constant.CHANGE_ISLOADING, { isloading: true });
+
         const currentPageNo = store.state.contactlist.pageno;
         contactAPI.updateContact(store.state.contact).then((response) => {
             if (response.data.status === 'success') {
@@ -36,20 +46,27 @@ export default {
         });
     },
     [Constant.UPDATE_PHOTO]: (store, payload) => {
+        store.dispatch(Constant.CHANGE_ISLOADING, { isloading: true });
+
         const currentPageNo = store.state.contactlist.pageno;
         contactAPI.updatePhoto(payload.no, payload.file).then(() => {
             store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
         });
     },
     [Constant.DELETE_CONTACT]: (store, payload) => {
+        store.dispatch(Constant.CHANGE_ISLOADING, { isloading: true });
+
         const currentPageNo = store.state.contactlist.pageno;
         contactAPI.deleteContact(payload.no).then(() => {
             store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
         });
     },
     [Constant.FETCH_CONTACT_ONE]: (store, payload) => {
+        store.dispatch(Constant.CHANGE_ISLOADING, { isloading: true });
+
         contactAPI.fetchContactOne(payload.no).then((response) => {
             store.commit(Constant.FETCH_CONTACT_ONE, { contact: response.data });
+            store.dispatch(Constant.CHANGE_ISLOADING, { isloading: false });
         });
     },
     [Constant.INITIALIZE_CONTACT_ONE]: (store) => {
