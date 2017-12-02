35784b1f7566d3eccf0b4a7768a7e8a1507e3290
KunhoLee <gunho1020@gmail.com>
2017-12-02 15:25:26 +0900

    예제 A-07: test/unit/specs/List.spec.js에 코드 추가

--- EX14_appendixA/todolistapp_test1/test/unit/specs/List.spec.js
+++ EX14_appendixA/todolistapp_test1/test/unit/specs/List.spec.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import store from '@/store';
 import List from '@/components/List.vue';
-import constant from '@/constant';
+import Constant from '@/constant';
 /* eslint-enable */
 
 describe('List.vue', () => {
@@ -18,6 +18,27 @@ describe('List.vue', () => {
 
         Vue.nextTick().then(() => {
             expect(vm.$el.querySelectorAll('li').length).to.equal(4);
+            done();
+        }).catch(done);
+    });
+
+    it('새로운 todo 추가후 목록 확인', (done) => {
+        const vm = new Vue({
+            template: '<div><list></list></div>',
+            store,
+            components: {
+                list: List
+            }
+        }).$mount();
+
+        vm.$store.dispatch(Constant.ADD_TODO, { todo: '스쿼시' });
+        vm.$store.dispatch(Constant.ADD_TODO, { todo: '수영' });
+
+        Vue.nextTick().then(() => {
+            const list = vm.$el.querySelectorAll('li');
+            expect(list[list.length - 1].textContent).to.contain('수영');
+            expect(list[list.length - 2].textContent).to.contain('스쿼시');
+            expect(list.length).to.equal(6);
             done();
         }).catch(done);
     });
