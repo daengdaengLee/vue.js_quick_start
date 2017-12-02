8fb92481fe5ee32bbd8af664def169f4b134d2fa
KunhoLee <gunho1020@gmail.com>
2017-12-02 15:52:55 +0900

    예제 A-08: test/unit/specs/List.spec.js 변경

--- EX14_appendixA/todolistapp_test1/test/unit/specs/List.spec.js
+++ EX14_appendixA/todolistapp_test1/test/unit/specs/List.spec.js
@@ -3,19 +3,21 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import store from '@/store';
 import List from '@/components/List.vue';
+import inputTodo from '@/components/inputTodo.vue';
 import Constant from '@/constant';
 /* eslint-enable */
 
+const vm = new Vue({
+    template: '<div><input-todo ref="a"></input-todo><list></list></div>',
+    store,
+    components: {
+        list: List,
+        'input-todo': inputTodo
+    }
+}).$mount();
+
 describe('List.vue', () => {
     it('초기 렌더링 화면 테스트', (done) => {
-        const vm = new Vue({
-            template: '<div><list></list></div>',
-            store,
-            components: {
-                list: List
-            }
-        }).$mount();
-
         Vue.nextTick().then(() => {
             expect(vm.$el.querySelectorAll('li').length).to.equal(4);
             done();
@@ -23,22 +25,17 @@ describe('List.vue', () => {
     });
 
     it('새로운 todo 추가후 목록 확인', (done) => {
-        const vm = new Vue({
-            template: '<div><list></list></div>',
-            store,
-            components: {
-                list: List
-            }
-        }).$mount();
+        vm.$refs.a._data.todo = '수영강습'; // eslint-disable-line no-underscore-dangle
 
-        vm.$store.dispatch(Constant.ADD_TODO, { todo: '스쿼시' });
-        vm.$store.dispatch(Constant.ADD_TODO, { todo: '수영' });
+        const evtClick = new window.Event('click');
+        const addbutton = vm.$el.querySelector('span.addbutton');
+        addbutton.dispatchEvent(evtClick);
+        vm._watcher.run(); // eslint-disable-line no-underscore-dangle
 
         Vue.nextTick().then(() => {
             const list = vm.$el.querySelectorAll('li');
-            expect(list[list.length - 1].textContent).to.contain('수영');
-            expect(list[list.length - 2].textContent).to.contain('스쿼시');
-            expect(list.length).to.equal(6);
+            expect(list[list.length - 1].textContent).to.contain('수영강습');
+            expect(list.length).to.equal(5);
             done();
         }).catch(done);
     });
